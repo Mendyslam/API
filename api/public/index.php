887d9254ddd8079f3434e73e0540ef3119a4c079
Mendyslam <mendieuwemedimo@gmail.com>
2021-08-29 04:31:57 -0700

    Access-Control header added

--- api/public/index.php
+++ api/public/index.php
@@ -10,6 +10,7 @@ $app = new App();
 
 if(isset($_GET['url'])) {
     header('content-type:application/json;charset=utf-8');
+    header('Access-Control-Allow-Origin: *');
     echo $app->result;
 }
 
